272de5423a33cb80bedd8c4ab2ac0526cbd2b728
rupak2001 <75200313+rupak2001@users.noreply.github.com>
2021-04-04 14:04:48 +0530

    Update db.js

--- back/db.js
+++ back/db.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://admin:rups1957@eg1.5ifzv.mongodb.net/db1?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true})
+mongoose.connect('mongodb://localhost:27017/db1',{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true})
 .then(()=>console.log('connected to db'));
 
-//mongodb+srv://admin:rups1957@eg1.5ifzv.mongodb.net/db1?retryWrites=true&w=majority
-//mongodb://localhost:27017/db1
